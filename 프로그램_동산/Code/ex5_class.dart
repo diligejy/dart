45f6d43cc4d1c588778518bf536c1a6da8c1e2a4
diligejy <sjy049@gmail.com>
2024-02-08 23:51:42 +0900

    TIL

--- 프로그램_동산/Code/ex5_class.dart
+++ 프로그램_동산/Code/ex5_class.dart
@@ -1,3 +1,22 @@
+class Stock {
+  final String name;
+  final double price;
+
+  Stock({
+    required this.name,
+    required this.price
+  });
+
+  void printInfo(){
+    print("Stock: $name, Price: \$${price.toStringAsFixed(2)}");
+  }
+}
+
 void main(){
-  
+  Stock stock1 = Stock(name: "Apple Inc", price: 150.00);
+  Stock stock2 = Stock(name: "Tesla Inc", price: 200.00);
+
+  stock1.printInfo();
+  stock2.printInfo();
+
 }
